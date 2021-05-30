1cd21ff14a8a23baa20e4fbfe9068ead08541d50
SgOrigin999 <you@example.com>
2021-05-30 18:26:29 +0530

    Add Part time Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -31,11 +31,25 @@ public class EmployeeWage {
 		}
 	}
 	
+	public static void partTimeWage() {
+		int totalWage = 0;
+		int partTimrHrs=8;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == 1) {
+			totalWage = WAGEPERHOUR * partTimrHrs;
+			System.out.println("Employee PartTimeIf Present Wage::" + totalWage);
+		} else {
+			System.out.println("Employee Absent Then Wage::" + totalWage);
+		}
+	}
+	
 public static void main(String[] args) {
 	System.out.println("Welcome To Employyee Wage Computation::");
 	System.out.println("Check Employee Avilablity::");
 	checkAvilablity();
 	System.out.println("Calculate Daily Wage::");
 	calculateDailyWage();
+	System.out.println("Add Part Time wage");
+	partTimeWage();	
 }
 }
