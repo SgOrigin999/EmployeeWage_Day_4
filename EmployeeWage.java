0f6f80fc26ba80a23a13db388136898883091260
SgOrigin999 <you@example.com>
2021-05-30 16:52:07 +0530

    Computaation Of Employee Wage

---
+++ EmployeeWage.java
@@ -0,0 +1,7 @@
+package com.bridgelab.employeewage.day4;
+
+public class EmployeeWage {
+public static void main(String[] args) {
+	System.out.println("Welcome To Employyee Wage Computation::");
+}
+}
